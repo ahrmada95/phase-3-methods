0da99e2f3d472f3ac938777bc03558adaf706dc5
Ahnaf <ahrmada95@gmail.com>
2022-08-16 15:34:33 -0400

    finished

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
-# Your code here!
+
+def greet_programmer 
+    return puts("Hello, programmer!")
+end
+
+def greet (name)
+    return puts("Hello, #{name}!")
+end
+
+def greet_with_default (name="programmer")
+    return puts("Hello, #{name}!")
+end
+
+def add (addend_1, addend_2)
+    return addend_1 + addend_2
+end
+
+def halve (dividend)
+    if (dividend.class == Integer) 
+        return dividend / 2
+    else 
+        return nil
+    end
+end
